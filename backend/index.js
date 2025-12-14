452d08386f46590ce86d78bf665708437310b13a
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-12-15 03:57:03 +0530

    fixexd errors in cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import noteRoutes from './routes/note.route.js'
 
 
@@ -11,21 +12,22 @@ const port = process.env.PORT || 4002
 
 //Database Connection
 const connectDB = async () => {
-    try{
+    try {
         await mongoose.connect(process.env.MONGO_URL)
         console.log("Connected to MongoDB")
-    }catch(error){
+    } catch (error) {
         console.log(`Error connecting to MongoDB, ${error}`)
     }
 }
 connectDB()
 
 //Routing Middelware
+app.use(cors())
 app.use(express.json())
 app.use("/api/v1/noteapp", noteRoutes)
 
 
 //
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`running on port ${port}`)
 })
